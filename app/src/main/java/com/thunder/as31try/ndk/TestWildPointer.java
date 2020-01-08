ef1534cf2dac67813c278f8464db22cff75f2cd4
heyuwen <heyuwen@thunder.com.cn>
2020-01-08 23:29:27 +0800

    NDK测试C语言

---
+++ app/src/main/java/com/thunder/as31try/ndk/TestWildPointer.java
@@ -0,0 +1,16 @@
+package com.thunder.as31try.ndk;
+
+/**
+ * 测试native野指针
+ */
+public class TestWildPointer {
+    native String getString(int len);
+
+    public static void main(String[] args) {
+        System.load("//Users//heyuwen//Documents//work//clion//two//cmake-build-debug//libtwo.dylib");
+        TestWildPointer testWildPointer = new TestWildPointer();
+        testWildPointer.getString(10);
+        testWildPointer.getString(20);
+    }
+
+}
